38924356d51ccaa0ab40fb5f3a8179f8aa8af2b2
Your Name <you@example.com>
2017-06-26 16:53:53 -0700

    first commit

---
+++ src/pages/login/login.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { NavController, ToastController } from 'ionic-angular';
+
+import { MainPage } from '../../pages/pages';
+
+import { User } from '../../providers/user';
+
+import { TranslateService } from '@ngx-translate/core';
+
+
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html'
+})
+export class LoginPage {
+  // The account fields for the login form.
+  // If you're using the username field with or without email, make
+  // sure to add it to the type
+  account: { email: string, password: string } = {
+    email: 'test@example.com',
+    password: 'test'
+  };
+
+  // Our translated text strings
+  private loginErrorString: string;
+
+  constructor(public navCtrl: NavController,
+    public user: User,
+    public toastCtrl: ToastController,
+    public translateService: TranslateService) {
+
+    this.translateService.get('LOGIN_ERROR').subscribe((value) => {
+      this.loginErrorString = value;
+    })
+  }
+
+  // Attempt to login in through our User service
+  doLogin() {
+    this.user.signin(this.account.email,this.account.password).subscribe((resp) => {
+      this.navCtrl.push(MainPage);
+    }, (err) => {
+      let toast = this.toastCtrl.create({
+        message: this.loginErrorString,
+        duration: 3000,
+        position: 'top'
+      });
+      toast.present();
+    });
+  }
+}
